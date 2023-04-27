a59d98b2728d7631c3e660448cffa9d04c233278
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-27 14:55:05 +0700

    Initial Commit

---
+++ MVVM_Master_Detail_Example/MVVM_Master_Detail_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_Master_Detail_ExampleApp.swift
+//  MVVM_Master_Detail_Example
+//
+//  Created by Pham Nguyen Phu on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Master_Detail_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
